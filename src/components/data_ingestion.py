b2ac1da63ff0dd8eed5b73c67ff4b4913dcb2b50
Ritik Kohad <kohadritik@gmail.com>
2023-12-05 14:44:44 +0530

    Modified data_transformation and data_ingestion python file

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,10 +7,10 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
-# from src.components.data_transformation import (
-#     DataTransformation,
-#     DataTransformationConfig,
-# )
+from src.components.data_transformation import (
+    DataTransformation,
+    DataTransformationConfig,
+)
 
 # from src.components.model_trainer import ModelTrainerConfig, ModelTrainer
 
@@ -68,10 +68,11 @@ if __name__ == "__main__":
     obj = DataIngestion()
     train_data, test_data = obj.initiate_data_ingestion()
 
-    # data_transformation = DataTransformation()
-    # train_array, test_array, _ = data_transformation.initiate_data_transformation(
-    #     train_data, test_data
-    # )
+    data_transformation = DataTransformation()
+
+    train_array, test_array, _ = data_transformation.initiate_data_transformation(
+        train_data, test_data
+    )
 
     # modeltraner = ModelTrainer()
     # print(
--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -11,7 +11,8 @@ from sklearn.preprocessing import OneHotEncoder, StandardScaler
 
 from src.exception import CustomException
 from src.logger import logging
-from src.utils import save_object
+
+# from src.utils import save_object
 
 
 @dataclass
@@ -44,7 +45,7 @@ class DataTransformation:
             cat_pipeline = Pipeline(
                 steps=[
                     ("imputer", SimpleImputer(strategy="most_frequent")),
-                    ("one_hot_encoder", OneHotEncoder()),
+                    ("one_hot_encoder", OneHotEncoder(drop="first")),
                 ]
             )
             logging.info("Numerical columns standard scaling completed")
@@ -65,14 +66,14 @@ class DataTransformation:
 
     def initiate_data_transformation(self, train_path, test_path):
         try:
+            logging.info("Reading train and test data initiated")
             train_df = pd.read_csv(train_path)
             test_df = pd.read_csv(test_path)
-
-            logging.info("Read train and test data completed")
+            logging.info("Reading train and test data completed")
 
             logging.info("Obtaining preprocessing object")
-
             preprocessing_obj = self.get_data_transformer_object()
+            logging.info("Preprocessor object obtained successfully")
 
             target_column_name = "math_score"
             numerical_columns = ["writing_score", "reading_score"]
@@ -86,23 +87,29 @@ class DataTransformation:
             logging.info(
                 "Applying preprocessing object on training dataframe and testing dataframe"
             )
-
             input_feature_train_arr = preprocessing_obj.fit_transform(
                 input_feature_train_df
             )
             input_feature_test_arr = preprocessing_obj.transform(input_feature_test_df)
+            logging.info(
+                "Successfully applied preprocessing object on train and test dataframe"
+            )
 
+            logging.info("Concatenating features and labels for train and test dataset")
             train_arr = np.c_[
                 input_feature_train_arr, np.array(target_feature_train_df)
             ]
             test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
-
-            logging.info("Saved preprocessing object")
-
-            save_object(
-                file_path=self.data_transformation_config.preprocessor_obj_file_path,
-                obj=preprocessing_obj,
+            logging.info(
+                "Concatenation features and labels for train and test dataset complete"
             )
+
+            # logging.info("Saved preprocessing object")
+
+            # save_object(
+            #     file_path=self.data_transformation_config.preprocessor_obj_file_path,
+            #     obj=preprocessing_obj,
+            # )
 
             return (
                 train_arr,
